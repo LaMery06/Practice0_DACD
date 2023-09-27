7029c0a5639fbd14225ead87f33c5d2598210646
maria <maria.alonso158@alu.ulpgc.es>
2023-09-27 18:30:08 +0100

    Revert "completed chessboard requirement"
    This reverts commit e737a73b44c21b92319c9f5fa8ad73896a39eb7e.

--- 5. ArrayList and 2D Arrays/2D Arrays/5.3 Chessboard/src/Chessboard.java
+++ 5. ArrayList and 2D Arrays/2D Arrays/5.3 Chessboard/src/Chessboard.java
@@ -4,24 +4,6 @@ public class Chessboard
    {
       char[][] chessboard = new char[8][8];
 
-      for (int y = 0; y < 8; y++)
-      {
-         for (int x = 0; x < 8; x++)
-         {
-            if ((x + y)%2 == 0)
-               chessboard[y][x] = '\u25A1';
-            else
-               chessboard[y][x] = '\u25A0';
-         }
-      }
-
-      for (char[] row: chessboard)
-      {
-         for (char cell: row)
-         {
-            System.out.print(cell);
-         }
-         System.out.println();
-      }
+      //TODO chessboard
    }
 }
